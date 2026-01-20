5b0d553e48d7d0eaad29e5f1ee2125ed1cb8f952
PreetamAnvekar <preetamanvekar2000@gmail.com>
2026-01-20 23:32:33 +0530

    Create base.py

---
+++ app/db/base.py
@@ -0,0 +1,13 @@
+from app.db.session import Base
+
+# Import all models here
+from app.models.admin_users import AdminUser
+from app.models.admin_details import AdminDetails
+from app.models.categories import Category
+from app.models.products import Product
+from app.models.cart import Cart
+from app.models.enquiries import Enquiry
+from app.models.enquiry_items import EnquiryItem
+from app.models.email_logs import EmailLog
+from app.models.user_visits import UserVisit
+from app.models.admin_activity_logs import AdminActivityLog

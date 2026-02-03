acaf691b077f20b904878ce2b88aab6686f45608
PreetamAnvekar <preetamanvekar2000@gmail.com>
2026-02-03 19:56:07 +0530

    Change environment setting from local to production

--- app/core/config.py
+++ app/core/config.py
@@ -3,12 +3,13 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Wholesale Stationery API"
-    ENV: str = "local"
+    ENV: str = "production"
     DATABASE_URL: str 
     SMTP_HOST: str
     SMTP_PORT: int
     SMTP_USER: str
     SMTP_PASSWORD: str
+    BREVO_API_KEY: str
     ADMIN_EMAIL: str
 
     class Config:
